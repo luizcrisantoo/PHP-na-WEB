f24c38cbf3eb79a67eed21f040dcc7be8aa5ea9b
luizcrisantoo <luiz.crisanto@gmail.com>
2025-04-01 10:32:02 -0300

    remover-video

--- aluraplay/index.php
+++ aluraplay/index.php
@@ -37,23 +37,26 @@ $videoList = $pdo->query('SELECT * FROM videos;')->fetchAll(\PDO::FETCH_ASSOC);
 
     <ul class="videos__container">
         <?php foreach ($videoList as $video): ?>
-        <?php if (str_starts_with($video['url'], 'http')): ?>
-        <li class="videos__item">
-            <iframe width="100%" height="72%" src="<?php echo $video['url']; ?>"
-                title="YouTube video player" frameborder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowfullscreen></iframe>
-            <div class="descricao-video">
-                <h3><?php echo $video['title']; ?></h3>
-                <div class="acoes-video">
-                    <a href="./pages/enviar-video.html">Editar</a>
-                    <a href="./pages/enviar-video.html">Excluir</a>
+        <?php
+          if (!str_starts_with($video['url'], 'http')) {
+            $video['url'] = 'https://www.youtube.com/embed/CujS0VGF8oA';
+            }
+            ?>
+            <li class="videos__item">
+                <iframe width="100%" height="72%" src="<?= $video['url']; ?>"
+                    title="YouTube video player" frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen></iframe>
+                <div class="descricao-video">
+                    <h3><?= $video['title']; ?></h3>
+                    <div class="acoes-video">
+                        <a href="./pages/enviar-video.html">Editar</a>
+                        <a href="/remover-video.php?id=<?= $video['id']; ?>">Excluir</a>
+                    </div>
                 </div>
-            </div>
-        </li>
-        <?php endif; ?>
-        <?php endforeach; ?>
-    </ul>
-</body>
+            </li>
+            <?php endforeach; ?>
+        </ul>
+    </body>
 
 </html>
