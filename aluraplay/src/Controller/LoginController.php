631b9b47b8a10ebf0abbfc17479ef7d6d4750e84
luizcrisantoo <luiz.crisanto@gmail.com>
2025-05-09 11:09:54 -0300

    changes

--- aluraplay/src/Controller/LoginController.php
+++ aluraplay/src/Controller/LoginController.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Alura\Mvc\Controller;
 
 class LoginController implements Controller
@@ -9,14 +11,18 @@ class LoginController implements Controller
     public function __construct()
     {
         $dbPath = __DIR__ . '/../../banco.sqlite';
-        $this->pdo = new PDO("sqlite:$dbPath");
+        $this->pdo = new \PDO("sqlite:$dbPath");
     }
     public function processaRequisicao(): void
     {
         $email = filter_input(type:INPUT_POST, var_name: 'email', filter: FILTER_VALIDATE_EMAIL);
         $password = filter_input(type:INPUT_POST, var_name: 'password');
+
         $sql = 'SELECT * FROM users WHERE email = ?';
         $statement = $this->pdo->prepare($sql);
-        $statement->bindValue(1, email);
+        $statement->bindValue(1, $email);
+
+        $userData = $statement->fetch(\PDO::FETCH_ASSOC);
+        $correctPassword = password_verify($password, $userData['password']);
     }
 }
