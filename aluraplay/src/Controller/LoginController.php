3918448c311796c293147d8d5bc80e0a3b6d833c
luizcrisantoo <luiz.crisanto@gmail.com>
2025-05-19 17:17:11 -0300

    logout controller

--- aluraplay/src/Controller/LoginController.php
+++ aluraplay/src/Controller/LoginController.php
@@ -27,10 +27,12 @@ class LoginController implements Controller
         $userData = $statement->fetch(\PDO::FETCH_ASSOC);
         $correctPassword = password_verify($password, $userData['password'] ?? '');
 
+
         if ($correctPassword) {
-            header('Location: /');
+                $_SESSION['logado'] = true;
+        header('Location: /');
         } else {
-            header('Location: /login?sucesso=0');
+        header('Location: /login?sucesso=0');
         }
     }
 }
